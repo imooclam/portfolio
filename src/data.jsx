8f6086af8e8cb7517375cc66732cf5cedeec40a1
imooclam <dragon158man@gmail.com>
2023-03-20 21:32:54 +0200

    (Portfolio)

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
-import { SiJavascript } from 'react-icons/si';
-import { FaReact } from 'react-icons/fs';
-import { BsBootstrap } from 'react-icons/bs';
-import { BsGithub } from 'react-icons/Bs';
+// import { SiJavascript } from 'react-icons/si';
+// import { FaReact } from 'react-icons/fs';
+// import { BsBootstrap } from 'react-icons/bs';
+// import { BsGithub } from 'react-icons/Bs';
 
 export const data = [
   {
@@ -9,62 +9,62 @@ export const data = [
     img: 'omifood-app.png',
     link: 'https://omifood-app.netlify.app/',
     name: 'omifood',
-    icon: <SiJavascript />,
+    icon: 'JS',
   },
   {
     id: 2,
     img: 'signature.PNG',
     link: 'https://signature-app.netlify.app/',
     name: 'signature-app',
-    icon: <FaReact />,
+    icon: 'React',
   },
   {
     id: 3,
     img: 'menu.PNG',
     link: 'https://menu-foo.netlify.app/',
     name: 'Menu-app',
-    icon: <FaReact />,
+    icon: 'React',
   },
   {
     id: 4,
     img: 'Slider.PNG',
     link: 'https://slider-app.netlify.app/',
     name: 'Slider-app',
-    icon: <FaReact />,
+    icon: 'React',
   },
   {
     id: 5,
     img: 'navbar.PNG',
     link: 'https://navbar-app-v1.netlify.app/',
     name: 'Responsive-Navbar-app',
-    icon: <FaReact />,
+    icon: 'React',
   },
   {
     id: 6,
     img: 'Bootstrap.PNG',
     link: 'https://business-bs.netlify.app/',
     name: 'Responsive(ws)-Bootstrap',
-    icon: <BsBootstrap />,
+    icon: 'BS',
   },
   {
     id: 7,
     img: 'time-zoneExercise.PNG',
     link: 'https://time-zone-js.netlify.app/',
     name: 'Time-zone',
-    icon: <SiJavascript />,
+    icon: 'JS',
   },
   {
     id: 8,
     img: 'Dad-jokes.PNG',
     link: 'https://dad-joke-app-ajax.netlify.app/',
     name: 'Dad-jokes(Ajax)',
-    icon: <SiJavascript />,
+    icon: 'JS',
   },
   {
     id: 9,
     img: 'immer.PNG',
     link: 'https://counter-immer.netlify.app/',
     name: 'Counter (Reduce && immer)',
-    icon: <FaReact />,
+    icon: 'React',
   },
 ];
